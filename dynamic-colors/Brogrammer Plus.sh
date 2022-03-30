46cbce6e47c0492f71ee0cafbd8809953d1323dc
Jack Jiang <jackjyq@outlook.com>
2022-03-30 12:38:46 +0800

    add Brogrammer Plus theme

---
+++ dynamic-colors/Brogrammer Plus.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Brogrammer Plus
+printf "\033]4;0;#121212;1;#e74c3c;2;#2ecc71;3;#f1c40f;4;#3498db;5;#6c71c4;6;#3cc9d6;7;#fbfbfb;8;#373737;9;#ff5542;10;#3bfb9d;11;#ffd60d;12;#48c0ff;13;#9ea7ff;14;#51f1ff;15;#ffffff\007"
+printf "\033]10;#fbfbfb;#121212;#ffffff\007"
+printf "\033]17;#264f78\007"
+printf "\033]19;#fcfcfc\007"
+printf "\033]5;0;#fcfcfc\007"
---
+++ pantheonterminal/Brogrammer Plus.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Brogrammer Plus'
+cursor-color='#ffffff'
+foreground='#fbfbfb'
+background='rgba(18,18,18,.95)'
+palette='#121212:#e74c3c:#2ecc71:#f1c40f:#3498db:#6c71c4:#3cc9d6:#fbfbfb:#373737:#ff5542:#3bfb9d:#ffd60d:#48c0ff:#9ea7ff:#51f1ff:#ffffff'
+COLORS
